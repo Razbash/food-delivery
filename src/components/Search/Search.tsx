d302d9295b7e6cc7c411cf7fa315255959ac41a1
dbezzubov <dbezzubov@simtechdev.org>
2022-06-28 22:19:34 +0400

    Add SVG support and style the Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,9 +1,20 @@
+import searchImage from '../../assets/icons/search.svg';
+
 const Search = () => {
     return(
-        <input type="search"
-            className="search"
-            placeholder="Search"
-        />
+        <div className="search">
+            <input type="search"
+                className="search__input"
+                placeholder="Search"
+            />
+
+            <img src={searchImage}
+                alt="Search"
+                className="search__icon"
+                width="16"
+                height="16"
+            />
+        </div>
     )
 }
 
