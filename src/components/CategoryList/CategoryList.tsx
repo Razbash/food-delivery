795c3b37e228f34493bd40fa132ad7178a0931a3
dbezzubov <dbezzubov@simtechdev.org>
2022-07-02 19:23:21 +0400

    Create a list of categories component

---
+++ src/components/CategoryList/CategoryList.tsx
@@ -0,0 +1,75 @@
+import pizzaImage from '../../assets/images/categories/pizza.png';
+import burgerImage from '../../assets/images/categories/burger.png';
+import bbqImage from '../../assets/images/categories/bbq.png';
+import sushiImage from '../../assets/images/categories/sushi.png';
+import veganImage from '../../assets/images/categories/vegan.png';
+import dessertImage from '../../assets/images/categories/dessert.png';
+
+const CategoryList = () => {
+    interface ICategory {
+        id: number,
+        name: string,
+        icon: string,
+    }
+
+    const categories:Array<ICategory> = [
+        {
+            id: 0,
+            name: "Pizza",
+            icon: pizzaImage,
+        },
+        {
+            id: 1,
+            name: "Burger",
+            icon: burgerImage,
+        },
+        {
+            id: 2,
+            name: "BBQ",
+            icon: bbqImage,
+        },
+        {
+            id: 3,
+            name: "Sushi",
+            icon: sushiImage,
+        },
+        {
+            id: 4,
+            name: "Vegan",
+            icon: veganImage,
+        },
+        {
+            id: 5,
+            name: "Dessert",
+            icon: dessertImage,
+        }
+    ];
+
+    return(
+        <div className="categories-list">
+            {categories.map(element => {
+                const {name, icon} = element;
+
+                return(
+                    <div className="categories-list__item"
+                        onClick={(e) => e.currentTarget.classList.toggle("categories-list__item--selected")}
+                        key={element.id}
+                    >
+                        <img src={icon}
+                            alt={name}
+                            className="categories-list__image"
+                            width="24"
+                            height="24"
+                        />
+
+                        <span className="categories-list__name">
+                            {name}
+                        </span>
+                    </div>
+                )
+            })}
+        </div>
+    );
+}
+
+export default CategoryList;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header/Header";
 import PromoList from "../components/PromoList/PromoList";
+import CategoryList from "../components/CategoryList/CategoryList";
 
 const HomePage = () => {
     return(
@@ -8,6 +9,7 @@ const HomePage = () => {
 
             <div className="container">
                 <PromoList/>
+                <CategoryList/>
             </div>
         </div>
     )
