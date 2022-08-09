44d82c5e1135880efa533626d55532df702a275f
dbezzubov <dbezzubov@simtechdev.org>
2022-08-09 15:49:14 +0400

    Fix path to deals images

--- src/components/DealsList/DealsList.tsx
+++ src/components/DealsList/DealsList.tsx
@@ -1,15 +1,4 @@
 import DealsListItem from '../DealsListItem/DealsListItem';
-import IDeal from '../../interfaces/IDeal';
-
-import forVeganImage from '../../assets/images/deals/20-for-vegans.png';
-import forMealsImage from '../../assets/images/deals/5-for-meals.png';
-import freePepsiImage from '../../assets/images/deals/free-pepsi.png';
-import getPizzaImage from '../../assets/images/deals/get-pizza.png';
-import above300Image from '../../assets/images/deals/above-300.png';
-import forVegansImage from '../../assets/images/deals/30-for-vegans.png';
-import freeBurgerImage from '../../assets/images/deals/free-burger.png';
-import freeSushiImage from '../../assets/images/deals/free-sushi.png';
-import desertImage from '../../assets/images/deals/25-deserts.png';
 import { useEffect } from 'react';
 import { fetchDeals } from '../../store/actions/dealsActions';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
