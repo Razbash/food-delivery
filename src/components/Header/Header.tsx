a2cea9bd5b54e995a56782217bf269924a369fa0
dbezzubov <dbezzubov@simtechdev.org>
2022-10-13 20:41:03 +0400

    Add mobile menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,55 @@
 import Logo from "../Logo/Logo";
 import Search from "../Search/Search";
 import Menu from "../Menu/Menu";
+import HamburgerIcon from "../../assets/icons/HamburgerIcon";
+import CrossIcon from "../../assets/icons/CrossIcon";
+import { useState } from "react";
 
 interface IHeader {
-    type: string;
+    type: string
 }
 
 const Header = (props: IHeader) => {
-    return(
-        <div className="header">
-            <div className="container header__wrapper">
-                <div className="header__left">
-                    <Logo color={"black"}/>
+    const [isOpenMobileMenu, setIsOpenMobileMenu] = useState(false);
 
+    const openMobileMenu = () => {
+        setIsOpenMobileMenu(true);
+    }
+
+    const closeMobileMenu = () => {
+        setIsOpenMobileMenu(false);
+    }
+
+    return(
+        <>
+            <div className="header">
+                <div className="container header__wrapper">
+                    <Logo color={"black"}/>
                     {props.type === "user" ? <Search/> : null}
-                </div>
-                <div className="header__right">
                     <Menu type={props.type}/>
+
+                    <div className="header__icon-wrapper" onClick={openMobileMenu}>
+                        <HamburgerIcon/>
+                    </div>
                 </div>
             </div>
-        </div>
+
+            {isOpenMobileMenu ?
+                <div className="header header--mobile">
+                    <div className="container header__wrapper">
+                        <div className="header__title">
+                            <Logo color={"black"}/>
+
+                            <div className="header__icon-wrapper" onClick={closeMobileMenu}>
+                                <CrossIcon/>
+                            </div>
+                        </div>
+                        <Menu type={props.type}/>
+                        {props.type === "user" ? <Search/> : null}
+                    </div>
+                </div>
+             : null}
+        </>
     )
 }
 
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -67,7 +67,7 @@ const Menu = (props: IMenu) => {
                 </div>
                 <span className="menu__cart-counter">4</span>
             </div>
-            <Link to="/user">
+            <Link to="/user" className="menu__user">
                 <img src={userImage}
                     alt="User image"
                     className="menu__user-image"
