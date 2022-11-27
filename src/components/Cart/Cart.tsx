6a29e0e8b15b6adbefcfae2bf11e1b40006c1e2e
dbezzubov <dbezzubov@simtechdev.org>
2022-11-27 20:54:07 +0400

    Get the user shopping cart data

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from 'react';
 import TrashIcon from '../../assets/icons/TrashIcon';
-import image from '../../assets/images/menu/rollSet.png';
+import IProduct from '../../interfaces/IProduct';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { fetchProducts } from '../../store/actions/productsActions';
 import { getCookie } from '../../tools/cookie';
 
+interface ICart {
+    productId: number,
+    count: number,
+}
+
 const Cart = () => {
-    const [userCart, setUserCart] = useState(null);
+    const [userCart, setUserCart] = useState<ICart[] | []>([]);
     const [productList, setProductList] = useState([]);
+    const dispatch = useAppDispatch();
+    const {products} = useAppSelector(state => state.products)
 
     useEffect(() => {
         setUserCart(getCookie('cart'));
+        dispatch(fetchProducts());
     },[]);
 
     return(
@@ -17,60 +27,46 @@ const Cart = () => {
                 <div className="cart__block-item">
                     <div className="cart__block-item-title">
                         <h6 className="cart__block-item-title-text">Menu</h6>
-                        <span className="cart__block-item-title-counter">4 meals</span>
+                        <span className="cart__block-item-title-counter">0 meals</span>
                     </div>
                     <div className="cart__list">
-                        <div className="cart__list-item">
-                            <div style={{"backgroundImage": 'url(' + image + ')'}}
-                                className="cart__item-image"
-                            ></div>
+                        {products.map((product:IProduct) => {
+                            return(
+                                userCart.map((userCartItem:ICart) => {
+                                    if (product.id === userCartItem.productId) {
+                                        const {id, name, description, price, image} = product;
 
-                            <div className="cart__item-product-info">
-                                <span className="cart__item-name">Chicken & Ribs Combo</span>
-                                <p className="cart__item-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga hic quasi ducimus tempora explicabo quam placeat cumque, aut alias, assumenda voluptates pariatur quis, temporibus sed dolorum nobis tenetur. Quas, est.</p>
-                            </div>
+                                        return(
+                                            <div className="cart__list-item" key={id}>
+                                                <div style={{"backgroundImage": 'url(' + image + ')'}}
+                                                    className="cart__item-image"
+                                                ></div>
 
-                            <div className="cart__item-controls">
-                                {/* Вынести в компонент */}
-                                <div className="quantity">
-                                    <span className="quantity__decrease">-</span>
-                                    <span className="quantity__value">1</span>
-                                    <span className="quantity__increase">+</span>
-                                </div>
+                                                <div className="cart__item-product-info">
+                                                    <span className="cart__item-name">{name}</span>
+                                                    <p className="cart__item-description">{description}</p>
+                                                </div>
 
-                                <span className="cart__item-price">$12.40</span>
+                                                <div className="cart__item-controls">
+                                                    {/* Вынеси в комнонент */}
+                                                    <div className="quantity">
+                                                        <span className="quantity__decrease">-</span>
+                                                        <span className="quantity__value">{userCartItem.count}</span>
+                                                        <span className="quantity__increase">+</span>
+                                                    </div>
 
-                                <div className="cart__item-remove">
-                                    <TrashIcon/>
-                                </div>
-                            </div>
-                        </div>
+                                                    <span className="cart__item-price">${(userCartItem.count * price).toFixed(2)}</span>
 
-                        <div className="cart__list-item">
-                            <div style={{"backgroundImage": 'url(' + image + ')'}}
-                                className="cart__item-image"
-                            ></div>
-
-                            <div className="cart__item-product-info">
-                                <span className="cart__item-name">Chicken & Ribs Combo</span>
-                                <p className="cart__item-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga hic quasi ducimus tempora explicabo quam placeat cumque, aut alias, assumenda voluptates pariatur quis, temporibus sed dolorum nobis tenetur. Quas, est.</p>
-                            </div>
-
-                            <div className="cart__item-controls">
-                                {/* Вынести в компонент */}
-                                <div className="quantity">
-                                    <span className="quantity__decrease">-</span>
-                                    <span className="quantity__value">1</span>
-                                    <span className="quantity__increase">+</span>
-                                </div>
-
-                                <span className="cart__item-price">$12.40</span>
-
-                                <div className="cart__item-remove">
-                                    <TrashIcon/>
-                                </div>
-                            </div>
-                        </div>
+                                                    <div className="cart__item-remove">
+                                                        <TrashIcon/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    }
+                                })
+                            )
+                        })}
                     </div>
                 </div>
             </div>
